602c1fc17439a2b1171a0642d22a1d61ec478cb4
0123takaokeita <keita.takao@di-v.co.jp>
2020-08-26 17:45:55 +0900

    ストロングパラメーター

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -13,5 +13,19 @@ class ItemsController < ApplicationController
     # アクションのnewをコールすると、エラーメッセージが入った@itemが上書きされてしまうので注意
     render 'new'
   end
-  
+
+private
+  def item_params
+    params.require(:item).permit(
+      :image,
+      :name,
+      :info,
+      :category_id,
+      :sales_status_id,
+      :shipping_fee_status_id,
+      :prefecture_id,
+      :scheduled_delivery_id,
+      :price
+    ).merge(user_id: current_user.id)
+  end
 end

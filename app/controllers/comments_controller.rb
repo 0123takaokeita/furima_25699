a4798bd495e76a2e3a5251cc2a6730518f7b7512
0123takaokeita <keita.takao@di-v.co.jp>
2020-11-02 15:09:25 +0900

    コントローラー編集

--- app/controllers/comments_controller.rb
+++ app/controllers/comments_controller.rb
@@ -1,4 +1,16 @@
 class CommentsController < ApplicationController
   def create
+    @comment = Comment.new(comments_params)
+    if @comment.save
+                                    # 部屋の名前
+      ActionCable.server.broadcast "comments_channel", {comment: @comment, user: @comment.user}
+    end
+  end
+
+  private
+
+  def comments_params
+    params.require(:comment).permit(:content)
+      .merge(user_id: current_user.id, item_id: params[:item_id])
   end
 end

74cc3fe5c993b994ff26e9450d82ce2fdc02afa4
0123takaokeita <keita.takao@di-v.co.jp>
2020-11-02 16:10:51 +0900

    jsファイル読み込み設定

---
+++ app/javascript/preview.js
@@ -0,0 +1,5 @@
+window.addEventListener("DOMContentLoaded", () => {                  // 商品出品・編集のフォームを取得
+  const itemForm = document.querySelector('.items-sell-main');       // 商品出品・編集のフォームがないなら実行せずここで終了
+  if (!itemForm) return null;                                        //itemFormが空だったらnull
+  console.log('preview.js');                                         //取得確認
+});

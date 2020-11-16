9350d50aea7950bad75455a53bb0cf04519d2da9
0123takaokeita <keita.takao@di-v.co.jp>
2020-11-16 18:48:14 +0900

    ビューとルーティング設定

--- config/routes.rb
+++ config/routes.rb
@@ -6,6 +6,10 @@ Rails.application.routes.draw do
   devise_for :users, :controllers => {
     :registrations => 'users/registrations'
   }
+  devise_scope :user do
+    get 'users/new_address_preset', to: 'users/registrations#new_address_preset'
+    post 'users/create_address_preset', to: 'users/registrations#create_address_preset'
+  end
   resources :items do
     resources :transactions, only: [:index, :create]
     resources :comments, only: :create     # 追記

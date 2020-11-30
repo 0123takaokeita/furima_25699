ecf89327abdbb93e582d90cc46075ada59cf1a19
0123takaokeita <keita.takao@di-v.co.jp>
2020-11-30 18:41:51 +0900

    キー追加

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,7 +1,6 @@
 
 class User < ApplicationRecord
-  devise :database_authenticatable, :registerable, :recoverable, :rememberable, :validatable
-
+  devise :database_authenticatable, :registerable, :recoverable, :rememberable, :validatable, :omniauthable, omniauth_providers: [:facebook, :google_oauth2]
   # <<バリデーション>>
   validates :nickname, presence: true, uniqueness: { case_sensitive: true }
 

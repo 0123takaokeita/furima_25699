59d824859f3038e5b19cee024757dc2272847985
0123takaokeita <keita.takao@di-v.co.jp>
2020-11-30 18:43:29 +0900

    ルーティング修正

--- config/routes.rb
+++ config/routes.rb
@@ -4,6 +4,7 @@ Rails.application.routes.draw do
   root to: "items#index"
   get 'items/new'
   devise_for :users, :controllers => {
+    :omniauth_callbacks => 'users/omniauth_callbacks',
     :registrations => 'users/registrations'
   }
   devise_scope :user do
